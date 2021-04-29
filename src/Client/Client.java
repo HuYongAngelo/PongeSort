9badc04b8f2d09cbdcb0279e3f5c19b695cc49aa
hu.yongan <hu.yongan@TAVE-PC20.OSDBSESTO.local>
2021-04-29 14:06:32 +0200

    Sends serialized char[] to Server

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -3,6 +3,7 @@ package Client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -13,8 +14,11 @@ public class Client {
 
     public static void main(String[] args) {
         String alfabeto = "abcdefghilmnopqrstuvz";
+        boolean controllo = true;
+        int tentativi = 0;
         
         char[] lista = new char[10];
+        char[] risposta;
         
         for (int i=0; i<lista.length; i++) {
             lista[i] = alfabeto.charAt((int) (Math.random()*alfabeto.length()));
@@ -23,14 +27,32 @@ public class Client {
         try {
             Socket server = new Socket("127.0.0.1", 5500);
             
-            PrintWriter out = new PrintWriter(server.getOutputStream(), true);
-            
             ObjectOutputStream oos = new ObjectOutputStream(server.getOutputStream());
-            oos.writeObject(lista);
+            ObjectInputStream ois = new ObjectInputStream(server.getInputStream());
             
-            out.close();
+            do {
+                lista = (char[])ois.readObject();
+                
+                for (int i=0; i<lista.length-1; i++) {
+                    for (int j=i; j<lista.length; j++) {
+                        if (lista[i]>lista[j]) {
+                            controllo = false;
+                            System.out.println("Ci è riuscito in "+tentativi+1+" tentativi!");
+                        } else {
+                            oos.writeObject(lista);
+                            tentativi++;
+                        }
+                    }
+                }
+            } while(controllo);
+            
+            
+            
+            ois.close();
             oos.close();
         } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
